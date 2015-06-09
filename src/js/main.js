f464d2210a90e0eaf0d4ecbac2c656592c8b5a6e
Carter Capocaccia <carter.capocaccia@gmail.com>
2015-06-08 19:45:40 -0500

    working edit/delete/add

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 angular
   .module('angularAddresses', ['ngRoute'])
 
+  .constant('API_URL', 'https://addressangular.firebaseio.com')
+
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -12,11 +14,28 @@ angular
       .when('/about', {
         templateUrl: 'views/about.html'
       })
+
       .when('/people', {
         templateUrl: 'views/people.html',
         controller: 'Main',
         controllerAs: 'main'
       })
+      .when('/people/new', {
+        templateUrl: 'views/people.html',
+        controller: 'NewPersonCtrl',
+        controllerAs: 'main'
+      })
+      .when('/people/:id', {
+        templateUrl: 'views/person.html',
+        controller: 'PersonController',
+        controllerAs: 'main'
+      })
+      .when('/people/:id/edit', {
+        templateUrl: 'views/person.html',
+        controller: 'EditPersonCtrl',
+        controllerAs: 'main'
+      })
+
       .otherwise({
         templateUrl: 'views/404.html'
       });
@@ -46,33 +65,108 @@ angular
     }
   })
 
-  .controller('Main', function ($http) {
+  .controller('PersonController', function ($routeParams, Person) {
+    var vm = this;
+    vm.id = $routeParams.id;
+
+    Person.getOne(vm.id, function (data) {
+      vm.person = data;
+    });
+
+    vm.destroy = function (id) {
+      Person.destroy(vm.id, function () {
+        window.location.href = '#/people';
+      });
+    };
+
+    vm.onModalLoad = function () {};
+  })
+
+  .factory('Person', function ($http, API_URL) {
+    return {
+      getOne(id, cb) {
+        $http
+          .get(`${API_URL}/people/${id}.json`)
+          .success(cb);
+      },
+
+      getAll(cb) {
+        $http
+          .get(`${API_URL}/people.json`)
+          .success(cb);
+      },
+
+      create(data, cb) {
+        $http
+          .post(`${API_URL}/people.json`, data)
+          .success(cb);
+      },
+
+      update(id, data, cb) {
+        $http
+          .put(`${API_URL}/people/${id}.json`, data)
+          .success(cb);
+      },
+
+      destroy(id, cb) {
+        $http
+          .delete(`${API_URL}/people/${id}.json`)
+          .success(cb);
+      }
+    }
+  })
+
+  .controller('NewPersonCtrl', function (Person) {
     var vm = this;
 
-    $http
-      .get('https://addressangular.firebaseio.com/people.json')
-      .success(function (data) {
-        vm.people = data;
+    vm.onModalLoad = function () {
+      $('#modal').modal('show');
+
+      $('#modal').on('hidden.bs.modal', function (e) {
+        window.location.href = '#/people';
       });
-
-    vm.newPerson = {};
-
-    vm.addNewAddress = function () {
-      $http
-        .post('https://addressangular.firebaseio.com/people.json', vm.newPerson)
-        .success(function (res) {
-          vm.people[res.name] = vm.newPerson;
-          vm.newPerson = {};
-          $('#modal').modal('hide');
-        });
     };
 
-    vm.removeAddress = function (id) {
-      $http
-        .delete(`https://addressangular.firebaseio.com/people/${id}.json`)
-        .success(function () {
-          delete vm.people[id]
-        });
+    vm.saveAddress = function () {
+      Person.create(vm.person, function () {
+        $('#modal').modal('hide');
+      });
     };
 
+    Person.getAll(function (people) {
+      vm.people = people;
+    });
+  })
+
+  .controller('EditPersonCtrl', function ($routeParams, Person) {
+    var vm = this;
+    vm.id = $routeParams.id;
+
+    vm.onModalLoad = function () {
+      $('#modal').modal('show');
+
+      $('#modal').on('hidden.bs.modal', function (e) {
+        window.location.href = `#/people/${vm.id}`;
+      });
+    };
+
+    vm.saveAddress = function () {
+      Person.update(vm.id, vm.person, function () {
+        $('#modal').modal('hide');
+      });
+    };
+
+    Person.getOne(vm.id, function (person) {
+      vm.person = person;
+    });
+  })
+
+  .controller('Main', function (Person) {
+    var vm = this;
+
+    Person.getAll(function (people) {
+      vm.people = people;
+    });
+
+    vm.onModalLoad = function () {};
   });
